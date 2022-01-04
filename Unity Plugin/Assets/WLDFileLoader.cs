0c8827c7fd830523d09db657e67b21f97a2aa77f
Christopher Stukes <chrisstukes@gmail.com>
2022-01-04 08:38:34 -0500

    1/4/21 update

--- Unity Plugin/Assets/WLDFileLoader.cs
+++ Unity Plugin/Assets/WLDFileLoader.cs
@@ -6,13 +6,13 @@ using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 
-public class WLDFileLoader : MonoBehaviour
-{
+public class WLDFileLoader : MonoBehaviour {
+
     private MeshFilter mfGroundInfo;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+
         FileStream fsAccess = null;
         byte[] abyFileData;
         byte[] abyHeightMap;
@@ -23,23 +23,30 @@ public class WLDFileLoader : MonoBehaviour
         long lHeightMapLen = 0;
         long lTxtrDataLen = 0;
         Regex rgChecker;
+        Material mtlCreate;
         List<Material> ltmtlGroundTxtrs = new List<Material>();
         string strFoundText = "";
-        string stGroundTxtrFileNames = "";
+        string strGroundTxtrFileNames = "";
         string strWaterTxtrFileName = "";
         string strSkyTxtrFileName = "";
+        string strCloudTxtrFileNames = "";
         List<string> ltstrCloudTxtrs = new List<string>();
         int nCharPos = 0;
         int nSideLen = 0;
-        int nCurrVertIndex = 0;
+        int nTxtrIndex = 0;
+        int nTxtrCheckIndex = 0;
+        int nTxtrDataIndex = -1;
+        int nStartIndex = 0;
+        List<Material> ltmtlGroundMapList = new List<Material>();
         List<Vector3> ltv3Verts = new List<Vector3>();
-        List<int> ltnTriages = new List<int>();
         List<Vector3> ltv3Nrmls = new List<Vector3>();
+        List<Vector3> ltv3SubmitVerts;
+        List<Vector3> ltv3SubmitNrmls;
         List<CombineInstance> ltciMeshes = new List<CombineInstance>();
-        List<Vector2> ltv2UV = new List<Vector2>();
 
         MeshRenderer mrGroundGen = gameObject.AddComponent<MeshRenderer>();
-        Mesh mhGround;
+
+        mfGroundInfo = gameObject.AddComponent<MeshFilter>();
 
         try {
 
@@ -76,15 +83,18 @@ public class WLDFileLoader : MonoBehaviour
 
                 if (strFoundText.Contains(".tga") || strFoundText.Contains(".dds")) {
 
-                    if (stGroundTxtrFileNames != "") {
+                    if (strGroundTxtrFileNames != "") {
 
-                        stGroundTxtrFileNames += ", ";
+                        strGroundTxtrFileNames += ", ";
                     }
 
-                    stGroundTxtrFileNames += strFoundText.Trim();
+                    strGroundTxtrFileNames += strFoundText.Trim();
 
-                    ltmtlGroundTxtrs.Add(Resources.Load("Assets/Textures/Environment/"  + 
-                                                        strFoundText.Trim().Replace(".tga", ".dds")) as Material);
+                    mtlCreate = new Material(Shader.Find("Standard"));
+                    mtlCreate.mainTexture = LoadTextureDXT("Assets/Textures/Environment/" +
+                                                           strFoundText.Trim().Replace(".tga", ".dds"));
+
+                    ltmtlGroundTxtrs.Add(mtlCreate);
                     strFoundText = "";
                 }
 
@@ -132,90 +142,95 @@ public class WLDFileLoader : MonoBehaviour
                     strFoundText = "";
                 }
 
-                if (strFoundText.Contains(".tga")) {
+                if (strFoundText.Contains(".tga") || strFoundText.Contains(".dds")) {
+                    
+                    if (strCloudTxtrFileNames != "") {
 
-                    ltstrCloudTxtrs.Add(strFoundText.Trim());
+                        strCloudTxtrFileNames += ", ";
+                    }
+
+                    strCloudTxtrFileNames += strFoundText.Trim();
+
+                    ltstrCloudTxtrs.Add(strFoundText.Trim().Replace(".tga", ".dds"));
                     strFoundText = "";
                 }
 
                 nCharPos++;
             }
 
-            //    int x = 0;
-            //    int y = 11;
-            //    int nDataIndex = y / 2 * nSideLen * 2 + x / 2 * 2;
-            //    Debug.Log(nDataIndex);
-            //    Debug.Log(abyHeightMap[nDataIndex]);
-            //    Debug.Log((int.Parse(abyHeightMap[nDataIndex].ToString()) - 10000) / 50);
-
             Debug.Log("Map Size: " + nMapSize);
-            Debug.Log("Ground Textures: " + String.Join(", ", stGroundTxtrFileNames));
+            Debug.Log("Ground Textures: " + String.Join(", ", strGroundTxtrFileNames));
             Debug.Log("Water Texture File: " + strWaterTxtrFileName);
             Debug.Log("Sky Texture: " + strSkyTxtrFileName);
-            Debug.Log("Cloud Textures: " + String.Join(", ", ltstrCloudTxtrs));
-
-            mfGroundInfo = gameObject.AddComponent<MeshFilter>();
-            mrGroundGen.sharedMaterial = new Material(Shader.Find("Standard"));
-            //            mrGroundGen.materials = ltmtlGroundTxtrs.ToArray();
-
-     //       int textNum = -1;
+            Debug.Log("Cloud Textures: " + strCloudTxtrFileNames);
 
             for (int nXPos = 0; nXPos < nMapSize; nXPos++) {
 
                 for (int nZPos = 0; nZPos < nMapSize; nZPos++) {
+
                     ltv3Verts.Add(new Vector3(nXPos,
                                               (int.Parse(abyHeightMap[nZPos / 2 * nSideLen * 2 + nXPos / 2 * 2].ToString()) - 10000) / 50,
                                               nZPos));
                     ltv3Nrmls.Add(-Vector3.forward);
 
- //                   if (textNum >= 255 && nZPos == 0) {
+                    if (nXPos % 2 == 0 && nZPos % 2 == 0) {
 
-//                        textNum = -1;
- //                   }
-
-  //                  if (textNum < 255 && textNum != int.Parse(abyTxtrData[nZPos * nSideLen + nXPos].ToString())) {
-
-    //                    Debug.Log("X: " + nXPos + ", Z: " + nZPos + ", ID: " + abyTxtrData[nZPos * nSideLen + nXPos].ToString());
-        //                textNum = int.Parse(abyTxtrData[nZPos * nSideLen + nXPos].ToString());
-      //              }
+                        nTxtrDataIndex++;
+                    }
 
                     if (nXPos > 0 && nZPos > 0) {
 
-                        nCurrVertIndex = ltv3Verts.Count - 1;
+                        nTxtrCheckIndex = int.Parse(abyTxtrData[nTxtrDataIndex + 1 - nSideLen + 1].ToString());
 
-                        ltnTriages.Add(nCurrVertIndex - 1);
-                        ltnTriages.Add((nCurrVertIndex - nMapSize) - 1);
-                        ltnTriages.Add(nCurrVertIndex - nMapSize);
-                        ltnTriages.Add(nCurrVertIndex - nMapSize);
-                        ltnTriages.Add(nCurrVertIndex);
-                        ltnTriages.Add(nCurrVertIndex - 1);
+                        if (nZPos >= nMapSize - 1 || 
+                            (nTxtrIndex != nTxtrCheckIndex &&
+                             nTxtrCheckIndex < ltmtlGroundTxtrs.Count && 
+                             nTxtrCheckIndex >= 0)) {
 
-                        if (nZPos >= nMapSize - 1) {
+                            if (nTxtrCheckIndex < ltmtlGroundTxtrs.Count && nTxtrCheckIndex >= 0) {
 
-                            ltciMeshes.Add(BuildGroundLane(ltv3Verts, ltnTriages, ltv3Nrmls, ltv2UV));
+                                nTxtrIndex = nTxtrCheckIndex;
+                            }
 
-                            ltv3Verts.RemoveRange(0, nMapSize);
-                            ltv3Nrmls.RemoveRange(0, nMapSize);
-                            ltnTriages.Clear();
-//                            ltv2UV.Clear();
+                            ltmtlGroundMapList.Add(ltmtlGroundTxtrs[nTxtrIndex]);
+                            
+                            ltv3SubmitVerts = ltv3Verts.GetRange(nStartIndex, (nZPos + 1) - nStartIndex);
+                            ltv3SubmitVerts.AddRange(ltv3Verts.GetRange(nStartIndex + nMapSize, (nZPos + 1) - nStartIndex));
+                            ltv3SubmitNrmls = ltv3Nrmls.GetRange(nStartIndex, (nZPos + 1) - nStartIndex);
+                            ltv3SubmitNrmls.AddRange(ltv3Nrmls.GetRange(nStartIndex + nMapSize, (nZPos + 1) - nStartIndex));
+
+                            ltciMeshes.Add(BuildGroundLane(ltv3SubmitVerts,
+                                                           GenerateTriangles(ltv3SubmitVerts.Count),
+                                                           ltv3SubmitNrmls,
+                                                           GenerateUV(ltv3SubmitVerts.Count)));
+
+                            if (nZPos >= nMapSize - 1) {
+
+                                ltv3Verts.RemoveRange(0, nMapSize);
+                                ltv3Nrmls.RemoveRange(0, nMapSize);
+                                nStartIndex = 0;
+                            }
+                            else { 
+
+                                nStartIndex = nZPos + 1;
+                            }
                         }
                     }
                 }
             }
 
-            if (ltv3Verts.Count > 0) { 
+            mrGroundGen.sharedMaterials = ltmtlGroundMapList.ToArray();
 
-                ltciMeshes.Add(BuildGroundLane(ltv3Verts, ltnTriages, ltv3Nrmls, ltv2UV));
-            }
-
-            mhGround = new Mesh();
+            Mesh mhGround = new Mesh();
             mhGround.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
-            mhGround.CombineMeshes(ltciMeshes.ToArray());
+
+            mhGround.CombineMeshes(ltciMeshes.ToArray(), false);
+
             mfGroundInfo.mesh = mhGround;
         }
         catch (Exception exError) {
 
-            Debug.Log("Method: Start, Action: Loading file, Exception: " + exError.Message);
+            Debug.Log("Method: Start, Action: Loading file, Exception: " + exError.Message + ". Stacktrace: " + exError.StackTrace);
         }
         finally {
 
@@ -227,8 +242,7 @@ public class WLDFileLoader : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 
@@ -240,15 +254,100 @@ public class WLDFileLoader : MonoBehaviour
         Mesh mhGround = new Mesh();
         CombineInstance ciMeshSelect = new CombineInstance();
 
-        mhGround.vertices = ltv3Verts.ToArray();
-        mhGround.triangles = ltnTriages.ToArray();
-        mhGround.normals = ltv3Nrmls.ToArray();
-//        mhGround.uv = ltv2UV.ToArray();
+        mhGround.SetVertices(ltv3Verts);
+        mhGround.SetTriangles(ltnTriages, 0);
+        mhGround.SetNormals(ltv3Nrmls);
+        mhGround.SetUVs(0, ltv2UV);
+        mhGround.Optimize();
         mfGroundInfo.mesh = mhGround;
 
         ciMeshSelect.mesh = mfGroundInfo.sharedMesh;
         ciMeshSelect.transform = mfGroundInfo.transform.localToWorldMatrix;
 
         return ciMeshSelect;
+    }
+
+    /// <summary>
+    ///     LoadTextureDXT (Courtesy of user: jeff-smith, site: https://answers.unity.com, 
+    ///                     page: https://answers.unity.com/questions/555984/can-you-load-dds-textures-during-runtime.html)
+    /// </summary>
+    Texture2D LoadTextureDXT(String strFilePathName) {
+
+        FileStream fsAccess = new FileStream(strFilePathName, FileMode.Open, FileAccess.Read);
+ 
+        byte[] ddsBytes = new byte[fsAccess.Length];
+        fsAccess.Read(ddsBytes, 0, (int)fsAccess.Length);
+        fsAccess.Close();
+
+        byte ddsSizeCheck = ddsBytes[4];
+        if (ddsSizeCheck != 124)
+            throw new Exception("Invalid DDS DXTn texture. Unable to read");  //this header byte should be 124 for DDS image files
+
+        int height = ddsBytes[13] * 256 + ddsBytes[12];
+        int width = ddsBytes[17] * 256 + ddsBytes[16];
+
+        int DDS_HEADER_SIZE = 128;
+        byte[] dxtBytes = new byte[ddsBytes.Length - DDS_HEADER_SIZE];
+        Buffer.BlockCopy(ddsBytes, DDS_HEADER_SIZE, dxtBytes, 0, ddsBytes.Length - DDS_HEADER_SIZE);
+
+        Texture2D texture = new Texture2D(width, height, TextureFormat.DXT5, false);
+        texture.LoadRawTextureData(dxtBytes);
+        texture.Apply();
+
+        return (texture);
+    }
+
+    List<int> GenerateTriangles(int nSize) {
+
+        List<int> ltnTriages = new List<int>();
+        int nSectionSize = nSize / 2;
+
+        try { 
+
+            for (int nCounter = nSectionSize + 1; nCounter < nSize; nCounter++) {
+
+                ltnTriages.Add(nCounter - 1);
+                ltnTriages.Add((nCounter - nSectionSize) - 1);
+                ltnTriages.Add(nCounter - nSectionSize);
+                ltnTriages.Add(nCounter - nSectionSize);
+                ltnTriages.Add(nCounter);
+                ltnTriages.Add(nCounter - 1);
+            }
+        }
+        catch (Exception exError) {
+
+            throw exError;
+        }
+
+        return ltnTriages;
+    }
+
+    List<Vector2> GenerateUV(int nVertListLen) {
+
+        List<Vector2> ltv2UV = new List<Vector2>();
+        int nSectionSize = nVertListLen / 2;
+        float fRowVal = 0;
+        float fTileVal = 0;
+
+        try { 
+
+            for (int nCounter = 0; nCounter < nVertListLen; nCounter++) {
+
+                if (nCounter == nSectionSize) {
+
+                    fRowVal = 1;
+                    fTileVal = 0;
+                }
+
+                ltv2UV.Add(new Vector2(fRowVal, fTileVal));
+                fTileVal += (float)0.5;
+            }
+        }
+        catch (Exception exError) {
+
+            throw exError;
+        }
+
+        return ltv2UV;
     }
 }
